80eceef25087f786acdd0ee282bb65f89f8f2d05
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-02-28 22:25:57 +0600

    pythonlesson

---
+++ index.py
@@ -0,0 +1,43 @@
+name = "jack \"sparrow\""
+surname = "O'Neil"
+# surname_upper = surname.upper()
+country = 'usa'
+print('hello,', name.title(), surname.upper())
+print('hello,' + name.title() + surname.upper(), country)
+print('hello, {} {}'.format(name, surname))
+print('hello, {name} {surname}')
+
+
+# matematika
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+number_one = 10
+number_two = 2
+print(number_one + number_two)
+print(number_one - number_two)
+print(number_one * number_two)
+print(number_one / number_two)
+
+print(number_one // number_two)  # bez ostatka drobi pri delenii
+print(number_one ** number_two)
+print(number_one % number_two)
+
+print(20 % 7)
+
+
+# matem
+print('50' + '50')  # izza skobok
+print(50 + 50)  # shikarno
+# print(type(name))
+
+# mojno
+var_1 = 'mojno'
+var_one = 'mojno'
+var_two = 'mojno'
+Var_1 = 'mojno no ne jelateno'
+
+# nelzya
+# 34b = 'nelzya'
+# b>ad$ = 'nelzya bydet ban'
+# and = 'nelzya'
---
+++ lesson_1_1.py
@@ -0,0 +1,76 @@
+# Встроенные функции, переменные, комментарии. строки, целые и дробные числа.
+# конкатенация (склеивание объектов)
+
+# str - string (строка)
+# int - integer (целочисленное)
+# float - floating (дробное)
+
+# i'm, O'Neil
+# print(type(name))
+# surname_upper = surname.upper()
+# print(int('5') + float('5'))
+
+# number_1 = 10
+# number_2 = 2
+#
+# print(2 + 3 * 4)
+# print((2 + 3) * 4)
+
+# print(int(10 / 2.0))
+
+# print(number_1 + number_2)
+# print(number_1 - number_2)
+# print(number_1 * number_2)
+# print(number_1 / number_2)
+#
+# print(number_1 // number_2)
+# print(number_1 ** number_2)
+# print(number_1 % number_2)
+# print(20 % 7)
+
+# print(type(age))
+# print(type(height))
+
+
+# sum_ages = 18 + 20 + 33 + 19 + 20 + 21 + 21 + 16 + 18 + 18 + 17 + 22 \
+#            + 20 + 28 + 21 + 23 + 20 + 15 + 41 + 15
+#
+# amount_students = 20
+#
+# average_age = sum_ages / amount_students
+# # average_age = int(average_age)
+# average_age = round(average_age, 2)
+# print(average_age)
+
+# print(round(20 / 7, 3))
+
+
+
+name = "jack \"sparrow\""
+surname = input('surname: ')  # фамилия
+age = int(input('age: '))
+height = 1.74
+current_year = 2023
+birth_year = current_year - age
+height = height + 0.02
+
+print(f'Hello, {name} {surname} height: {height} \n на земле с {birth_year} года')
+
+
+# print('Hello, ', name.title(), surname.upper(), country)
+# print('Hello, ' + name.title() + surname.upper(), country)
+# print('Hello, {} {}'.format(name, surname))
+
+
+# можно
+# var_1 = 'можно'
+# var_one = 'можно'
+# var_One = 'можно'
+# Var_One = 'можно'
+# var_1n = 'можно'
+
+# нельзя
+# 34bad = 'нельзя'
+# {b>ad$ = 'нельзя'
+# ba d = 'нельзя'
+# and = 'нельзя'
